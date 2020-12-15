75689582f563d69b27c306198932e1b0f79d3ea8
Marie-Astrid Chanteloup <marie-astrid.chanteloup@telecomnancy.eu>
2020-12-15 23:39:59 +0100

    interface de présentation partiellement fonctionnel (problème de chemin d'image)

--- src/sample/interfa/PresentParcoursAccueil.java
+++ src/sample/interfa/PresentParcoursAccueil.java
@@ -1,9 +1,9 @@
 package sample.interfa;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import sample.Parcours;
 import sample.VisitorVisualisation;
 
@@ -12,30 +12,33 @@ import java.util.ResourceBundle;
 
 
 public class PresentParcoursAccueil implements Initializable {
-    public Label level;
-    public Button name;
-    public Label descCourte;
-    public Label distance;
-    public Label duree;
-    public Label diff;
-    public Label descLongue;
+    public  Label level;
+    public  Button name;
+    public  Label descCourte;
+    public  Label distance;
+    public  Label duree;
+    public  Label diff;
+    public  Label descLongue;
+    public  ImageView imRando;
 
-    private Parcours parc;
-    private VisitorVisualisation vis;
+    public void myFunct(Parcours parc){
+        VisitorVisualisation vis = new VisitorVisualisation();
+        vis.visit(parc);
+        this.name.setText(vis.getName());
+        descCourte.setText(vis.getDescCourte());
+        distance.setText(vis.getDistance()+"km");
+        duree.setText(vis.getDuree()+"h");
+        level.setText(vis.getDiff()+"/5");
+        diff.setText(vis.getDenivele()+"m");
+        descLongue.setText(vis.getDescLongue());
 
-    public PresentParcoursAccueil(Parcours parc){
-        this.parc = parc;
-        this.vis = new VisitorVisualisation();
+        if (!vis.getPhotos().isEmpty()){
+            imRando.setImage(vis.getPhotos().get(0));
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        vis.visit(parc);
-        name.setText(vis.getName());
-        descCourte.setText(vis.getDescCourte());
-        distance.setText(vis.getDistance()+"km");
-        duree.setText(vis.getDuree()+"h");
-        diff.setText(vis.getDiff()+"5");
-        descLongue.setText(vis.getDescLongue());
+
     }
 }
