8eda716f8f6b295d39b477277328f823d41f26e2
Amadis LEMORE <amadis.lemore@telecomnancy.eu>
2020-12-16 15:55:03 +0100

    ajout de la doc dans sample.VisitorVisualisation et GPX.Reader

--- src/GPX/Reader.java
+++ src/GPX/Reader.java
@@ -21,6 +21,14 @@ public class Reader {
 	private DocumentBuilder db;
 	private Document doc;
 
+	/**
+	 * lecteur de fichier GPX
+	 * @param filePath
+	 * 		chemin du fichier à lire
+	 * @throws ParserConfigurationException
+	 * @throws IOException
+	 * @throws SAXException
+	 */
 	public Reader(String filePath) throws ParserConfigurationException, IOException, SAXException {
 		file = new File(filePath);
 		file = new File(getClass().getClassLoader().getResource(filePath).getFile());
@@ -30,6 +38,13 @@ public class Reader {
 		doc.getDocumentElement().normalize();
 	}
 
+	/**
+	 * renvoie un lecteur de GPX, catch les erreurs
+	 * @param filePath
+	 * 		chemin du fichier à lire
+	 * @return Reader
+	 * 		l'instance créé du lecteur
+	 */
 	public static Reader CreateReader(String filePath) {
 		try {
 			Reader r = new Reader(filePath);
@@ -44,10 +59,20 @@ public class Reader {
 		return null;
 	}
 
+	/**
+	 * @return Node
+	 * 		node racine du fichier
+	 */
 	public Node getRoot(){
 		return doc.getDocumentElement();
 	}
 
+	/**
+	 * @param tag
+	 * 		tag à vérifier
+	 * @return NodeList
+	 * 		liste des node avec le tag
+	 */
 	public NodeList getElementsByTag(String tag){
 		return doc.getElementsByTagName(tag);
 	}
@@ -64,6 +89,13 @@ public class Reader {
 		}
 	}
 
+	/**
+	 * renvoie la latitude de la node n
+	 * @param n
+	 * 		node à regarder
+	 * @return double
+	 * 		latitude de la node n, 0 si la node n'as pas de latitude
+	 */
 	static double getLatitude(Node n){
 		Element e = (Element) n;
 		if ( e.hasAttribute("lat")){
@@ -71,6 +103,13 @@ public class Reader {
 		}
 		return 0d;
 	}
+	/**
+	 * renvoie la longitude de la node n
+	 * @param n
+	 * 		node à regarder
+	 * @return double
+	 * 		longitude de la node n, 0 si la node n'as pas de longitude
+	 */
 	static double getLongitude(Node n){
 		Element e = (Element) n;
 		if ( e.hasAttribute("lon")){
@@ -78,6 +117,13 @@ public class Reader {
 		}
 		return 0d;
 	}
+	/**
+	 * renvoie l'élévation associé à la node n
+	 * @param n
+	 * 		node à regarder
+	 * @return double
+	 * 		élévation de la node n, 0 si la node n'as pas d'élévation
+	 */
 	static double getElevation(Node n){
 		Element e = (Element) n;
 		if ( e.getElementsByTagName("ele").getLength() > 0){
@@ -86,10 +132,22 @@ public class Reader {
 		return 0d;
 	}
 
+	/**
+	 * renvoie les coordonées rangé dans la node
+	 * @param n
+	 * 		node à regarder
+	 * @return Coordonees
+	 * 		coordonnées de la node n
+	 */
 	static Coordonees getCords(Node n){
 		return new Coordonees((float)getLatitude(n), (float)getLongitude(n), (float)getElevation(n));
 	}
 
+	/**
+	 *
+	 * @return Trace
+	 * 		trace correspondant aux coordonées des nodes du gpx
+	 */
 	public Trace getTrace(){
 		List<Coordonees> listCords = new ArrayList<Coordonees>();
 		NodeList list = this.getElementsByTag("wpt");
@@ -99,6 +157,11 @@ public class Reader {
 		return new Trace(listCords);
 	}
 
+	/**
+	 * conversion en format mapjfx de la trace contenu dans le gpx
+	 * @return List<Coordinate>
+	 *     	liste des coordonées contenu dans le fichier
+	 */
 	public List<Coordinate> getListCoordinates(){
 		return this.getTrace().getListCoordinates();
 	}
--- src/sample/VisitorVisualisation.java
+++ src/sample/VisitorVisualisation.java
@@ -24,6 +24,11 @@ public class VisitorVisualisation implements Visitor {
 	}
 
 	@Override
+	/**
+	 * override visite du parcours
+	 * @param parc
+	 * 		parcours à visiter
+	 */
 	public void visit(Parcours parc) {
 		this.name = parc.name;
 		this.descCourte = parc.descCourte;
@@ -37,23 +42,53 @@ public class VisitorVisualisation implements Visitor {
 		parc.fiche.accept(this);
 	}
 
+	/**
+	 * @return String
+	 * 		nom du parcours visité
+	 */
 	public String getName() {return name;}
-
+	/**
+	 * @return String
+	 * 		description courte du parcours visité
+	 */
 	public String getDescCourte() {	return descCourte;}
-
+	/**
+	 * @return String
+	 * 		description longue du parcours visité
+	 */
 	public String getDescLongue() {return descLongue;}
-
+	/**
+	 * @return List<Image>
+	 * 		liste des photos du parcours visité
+	 */
 	public List<Image> getPhotos() {return photos;}
-
+	/**
+	 * @return float
+	 * 		dénivelé total du parcours visité
+	 */
 	public float getDenivele() { return denivele; }
-
+	/**
+	 * @return int
+	 * 		difficulté noté sur 5 du parcours visité
+	 */
 	public int getDiff() { return diff;	}
-
+	/**
+	 * @return float
+	 * 		distance total du parcours visité
+	 */
 	public float getDistance() { return distance;}
-
+	/**
+	 * @return float
+	 * 		durée total du parcours visité
+	 */
 	public float getDuree() { return duree; }
 
 	@Override
+	/**
+	 * override visite de la fiche technique
+	 * @param fiche
+	 * 		fiche technique à visiter
+	 */
 	public void visit(FicheTech fiche) {
 		System.out.println("durée:"+fiche.duree);
 		System.out.println("distance:"+fiche.distance);
@@ -63,12 +98,22 @@ public class VisitorVisualisation implements Visitor {
 	}
 
 	@Override
+	/**
+	 * override visite de coordonée
+	 * @param cord
+	 * 		Coordonée à visiter
+	 */
 	public void visit(Coordonees cord) {
 		System.out.println("\tlat:"+ cord.lattitude+" long:"+cord.longitude+" elev:"+cord.elevation);
 		cord.accept(this);
 	}
 
 	@Override
+	/**
+	 * override visite du chemin
+	 * @param trace
+	 * 		Trace à visiter
+	 */
 	public void visit(Trace trace) {
 		System.out.println("chemin :");
 		trace.accept(this);
