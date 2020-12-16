72d3634f75be5d1d2c4bad953c749a8e938a3ce5
GRAINDORGE Bleunwenn <bleunwenn.graindorge@telecomnancy.eu>
2020-12-16 10:48:48 +0100

    fiche

--- src/sample/interfa/FicheTechControl.java
+++ src/sample/interfa/FicheTechControl.java
@@ -1,14 +1,18 @@
-package sample.control;
+package sample.interfa;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import sample.Parcours;
 import sample.Visitor;
 import sample.VisitorVisualisation;
 
 import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class FicheTechControl {
+public class FicheTechControl implements Initializable {
 
     @FXML
     private Label FicheName;
@@ -24,19 +28,30 @@ public class FicheTechControl {
     private Label difficulte;
     @FXML
     private Label denivele;
+    @FXML
+    private ImageView imRando0;
 
     public FicheTechControl(){}
 
-    public FicheTechControl(Parcours p){
+    public void myFichControl(Parcours p){
         VisitorVisualisation v = new VisitorVisualisation();
         v.visit(p);
         FicheName.setText(v.getName());
         FicheShortDescr.setText(v.getDescCourte());
         FicheLongDescr.setText(v.getDescLongue());
-        duree.setText(String. valueOf(v.getDuree()));
-        distance.setText(String. valueOf(v.getDistance()));
-        difficulte.setText(String. valueOf(v.getDiff()));
-        denivele.setText(String. valueOf(v.getDenivele()));
+        duree.setText(v.getDuree()+"h");
+        distance.setText(v.getDistance()+"km");
+        difficulte.setText(v.getDiff()+"/5");
+        denivele.setText(v.getDenivele()+"m");
+
+        if (!v.getPhotos().isEmpty()){
+            imRando0.setImage(v.getPhotos().get(0));
+        }
+
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
