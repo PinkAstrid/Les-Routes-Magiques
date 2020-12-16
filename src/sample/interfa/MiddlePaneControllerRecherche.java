5e376dd97029b9d8c8f4be40257da86883ca0adc
Coralie DELARBRE <coralie.delarbre@telecomnancy.eu>
2020-12-16 15:02:35 +0100

    interface pour afficher la recherche

---
+++ src/sample/interfa/MiddlePaneControllerRecherche.java
@@ -0,0 +1,33 @@
+package sample.interfa;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+import sample.Parcours;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class MiddlePaneControllerRecherche {
+    public BorderPane borPane;
+
+    public MiddlePaneControllerRecherche(){}
+
+    public void setBorPane(List<Parcours> parcoursList) throws IOException {
+        String fxmlFile = "./vBoxMiddlePaneAccueil.fxml"; //vers ta classe
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
+        Parent rootNode = fxmlLoader.load();
+
+        VBoxMiddlePaneAccueil controller = fxmlLoader.getController(); //type de ton controller
+        controller.initList(parcoursList); //la fonction permettant d'ajouter les éléments dans ton controller
+        borPane.setCenter(rootNode);
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
