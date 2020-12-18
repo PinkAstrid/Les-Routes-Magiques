da5f51a0838f31236c688683972386a0e8b4c57d
GRAINDORGE Bleunwenn <bleunwenn.graindorge@telecomnancy.eu>
2020-12-18 10:00:10 +0100

    documentation java

--- src/sample/Parcours.java
+++ src/sample/Parcours.java
@@ -15,6 +15,9 @@ public class Parcours implements ElementVisitor {
 	Trace trace;
 	private Boolean favoris; //true quand le parcours est dans les favoris
 
+	/**
+	 * Constructeur
+	 */
 	public Parcours() {
 		photos = new ArrayList<Image>();
 		waypoints = new ArrayList<Waypoint>();
@@ -22,6 +25,23 @@ public class Parcours implements ElementVisitor {
 		trace = new Trace();
 	}
 
+	/**
+	 * Constructeur avec paramètres
+	 * @param name
+	 * nom du parcours
+	 * @param description
+	 * description courte du parcours
+	 * @param details
+	 * description détaillée du parcours
+	 * @param photos
+	 * photos du parcours
+	 * @param fiche
+	 * fiche technique associée au parcours
+	 * @param trace
+	 * trace du parcours
+	 * @param waypoints
+	 * points d'arrêts du parcours
+	 */
 	public Parcours(String name, String description, String details, ArrayList<Image> photos, FicheTech fiche, Trace trace, List<Waypoint> waypoints){
 		this.name = name;
 		this.descCourte = description;
@@ -33,66 +53,143 @@ public class Parcours implements ElementVisitor {
 		this.favoris = false;
 	}
 
+	/**
+	 * @return
+	 * nom du parcours
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * mise à jour du nom du parcours
+	 * @param name
+	 * nouveau nom à donner au parcours
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return
+	 * description courte du parcours
+	 */
 	public String getDescCourte() {
 		return descCourte;
 	}
 
+	/**
+	 * mise à jour de la description courte du parcours
+	 * @param descCourte
+	 * nouvelle description à donner au parcours
+	 */
 	public void setDescCourte(String descCourte) {
 		this.descCourte = descCourte;
 	}
 
+	/**
+	 * @return
+	 * description détaillée du parcours
+	 */
 	public String getDescLongue() {
 		return descLongue;
 	}
 
+	/**
+	 * mise à jour de la description détaillée du parcours
+	 * @param descLongue
+	 * nouvelle description détaillée à donner au parcours
+	 */
 	public void setDescLongue(String descLongue) {
 		this.descLongue = descLongue;
 	}
 
+	/**
+	 * @return
+	 * liste de photos du parcours
+	 */
 	public List<Image> getPhotos() {
 		return photos;
 	}
 
+	/**
+	 * mise à jour des photos du parcours
+	 * @param photos
+	 * nouvelle liste de photos à donner au parcours
+	 */
 	public void setPhotos(List<Image> photos) {
 		this.photos = photos;
 	}
 
+	/**
+	 * @return
+	 * liste des points d'arrêts du parcours
+	 */
 	public List<Waypoint> getWaypoints() {
 		return waypoints;
 	}
 
+	/**
+	 * mise à jour des points d'arrêts du parcours
+	 * @param waypoints
+	 * nouvelle liste de points d'arrêts à donner au parcours
+	 */
 	public void setWaypoints(List<Waypoint> waypoints) {
 		this.waypoints = waypoints;
 	}
 
-	public void setFiche(FicheTech fiche) {
-		this.fiche = fiche;
-	}
-
-	public void setTrace(Trace trace) {
-		this.trace = trace;
-	}
-
-	public Trace getTrace(){
-		return trace;
-	}
-
+	/**
+	 * @return
+	 * fiche technique du parcours
+	 */
 	public FicheTech getFiche(){
 		return fiche;
 	}
 
+	/**
+	 * mise à jour de la fiche technique du parcours
+	 * @param fiche
+	 * nouvelle fiche technique à donner au parcours
+	 */
+	public void setFiche(FicheTech fiche) {
+		this.fiche = fiche;
+	}
+
+	/**
+	 * @return
+	 * trace du parcours
+	 */
+	public Trace getTrace(){
+		return trace;
+	}
+
+	/**
+	 * mise à jour de la trace du parcours
+	 * @param trace
+	 * nouvelle trace à donner au parcours
+	 */
+	public void setTrace(Trace trace) {
+		this.trace = trace;
+	}
+
+	/**
+	 * @return
+	 * booléen de favori. Retourne true si le parcours est dans les favoris, false sinon
+	 */
 	public Boolean getFavoris() {return favoris;}
 
+	/**
+	 * mise à jour du champs favoris du parcours
+	 * @param favoris
+	 * nouvelle valeur du favoris du parcours
+	 */
 	public void setFavoris(Boolean favoris) {this.favoris = favoris;}
 
+	/**
+	 * override accepte pour le parcours, doit accepter la fiche technique, l'ensemble des points d'arrêts et la trace
+	 * @param v
+	 * visiteur à accepter
+	 */
 	@Override
 	public void accept(Visitor v) {
 		v.visit(fiche);
